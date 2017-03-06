ebdb167fbdb140f8c4293bbe36612ca011335cc2
msccreater <lymsc2008@gmail.com>
2017-03-06 21:02:11 +0800

    树形导航条编写部分代码,还没有完善,正在编写中

--- TreeNavigationBar/treenavigationbar.cpp
+++ TreeNavigationBar/treenavigationbar.cpp
@@ -1,4 +1,140 @@
 ﻿#include "treenavigationbar.h"
+#include <QPainter>
+
+const int PaintingScaleFactor = 20;//折叠图形缩放比例
+const int TreeNodeExtraSpace = 50;//节点额外空间
+FoldShape::FoldShape()
+{
+    m_style = CrossStyle;
+    m_normalColor = Qt::black;
+    m_selectedColor = Qt::white;
+    Init();
+}
+
+FoldShape::FoldShape(FoldShape::FoldShapeStyle style, const QColor& normal_color, const QColor& select_color)
+{
+    m_style = style;
+    m_normalColor = normal_color;
+    m_selectedColor = select_color;
+    Init();
+}
+
+void FoldShape::paint(QPainter *painter, const QRect &rect) const
+{
+    painter->save();
+
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(m_isSelected?m_selectedColor:m_normalColor);
+
+    int yOffset = (rect.height() - PaintingScaleFactor) / 2;
+    painter->translate(rect.x(), rect.y() + yOffset);
+    painter->scale(PaintingScaleFactor, PaintingScaleFactor);
+    switch(m_style)
+    {
+    case CrossStyle:
+        painter->drawPolygon(m_crossPolygon, Qt::WindingFill);
+        break;
+    default:
+        break;
+    }
+
+    painter->restore();
+}
+
+QSize FoldShape::sizeHient() const
+{
+    return PaintingScaleFactor * QSize(myMaxStarCount, 1);
+}
+
+void FoldShape::setFoldState(bool is_fold)
+{
+    m_isFold = is_fold;
+}
+
+bool FoldShape::isFold() const
+{
+    return m_isFold;
+}
+
+void FoldShape::Init()
+{
+    m_isFold = true;
+    m_isSelected = false;
+    m_crossPolygon << QPointF(0.2, 0.4) << QPointF(0.4, 0.4) << QPointF(0.4, 0.2)
+                   << QPointF(0.6, 0.2) << QPointF(0.6, 0.4) << QPointF(0.8, 0.4)
+                   << QPointF(0.8, 0.6) << QPointF(0.6, 0.6) << QPointF(0.6, 0.8)
+                   << QPointF(0.4, 0.8) << QPointF(0.4, 0.6) << QPointF(0.2, 0.6)
+                   << QPointF(0.2, 0.4);
+}
+
+TreeNode::TreeNode()
+{
+    m_level = 0;
+}
+
+TreeNode::TreeNode(FoldShape foldShape, const QString &baseInfo, const QString &extraInfo)
+{
+    m_foldShape = foldShape;
+    m_baseInfo = baseInfo;
+    m_extraInfo = extraInfo;
+    m_level = 0;
+}
+
+TreeNode::~TreeNode()
+{
+    for(QList<TreeNode*>::iterator it=child.begin(); it!=child.end(); ++it)
+    {
+        delete(*it);
+        child.erase(it);
+    }
+}
+
+void TreeNode::addChild(TreeNode *node)
+{
+    if(node)
+    {
+        node->level = level+1;
+        child.append(node);
+    }
+}
+
+void TreeNodeDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
+                         const QModelIndex &index) const
+{
+    if (index.data().canConvert<TreeNode>()) {
+        TreeNode node = qvariant_cast<TreeNode>(index.data());
+        //填充背景色
+        QColor bgColor = node.normalColor();
+        if (option.state & QStyle::State_Selected)
+            bgColor = node.selectedColor();
+        else if(option.state & QStyle::State_MouseOver)
+            bgColor = node.hoverColor();
+        painter->fillRect(option.rect, bgColor);
+        //画折叠图标
+        QRect foldRect = option.rect;
+        foldRect.setWidth(PaintingScaleFactor);
+        node.foldShape().paint(painter, foldRect);
+        //画文字
+        QRect baseInfoRect =option.rect;
+        baseInfoRect.setLeft(PaintingScaleFactor);
+    } else {
+        QStyledItemDelegate::paint(painter, option, index);
+    }
+}
+
+QSize TreeNodeDelegate::sizeHint(const QStyleOptionViewItem &option,
+                             const QModelIndex &index) const
+{
+    if (index.data().canConvert<StarRating>()) {
+        StarRating starRating = qvariant_cast<StarRating>(index.data());
+        return starRating.sizeHint();
+    } else {
+        return QStyledItemDelegate::sizeHint(option, index);
+    }
+}
+
+
 
 TreeNavigationBar::TreeNavigationBar(QWidget *parent)
     : QWidget(parent)
@@ -10,22 +146,4 @@ TreeNavigationBar::~TreeNavigationBar()
 
 }
 
-TreeNode::TreeNode(QObject *parent)
-{
 
-}
-
-TreeNode::TreeNode(const QString &baseInfo, const QString &extraInfo, QObject *parent)
-{
-
-}
-
-TreeNode::~TreeNode()
-{
-
-}
-
-void TreeNode::addChild(TreeNode *child)
-{
-
-}
--- TreeNavigationBar/treenavigationbar.h
+++ TreeNavigationBar/treenavigationbar.h
@@ -2,28 +2,75 @@
 #define TREENAVIGATIONBAR_H
 
 #include <QWidget>
+#include <QMetaType>
+#include <QStyledItemDelegate>
 
-
-
-struct TreeNode : public QObject
+//折叠图标
+class FoldShape
 {
+public:
+    //折叠图形类型
     enum FoldShapeStyle
     {
         CrossStyle,//十字形
         TriangleStyle,//三角形
     };
+    FoldShape();
+    FoldShape(FoldShapeStyle style, const QColor& normal_color, const QColor& select_color);
+    void paint(QPainter *painter, const QRect &rect) const;
+    QSize sizeHient() const;
+    void setFoldState(bool is_fold);
+    bool isFold() const;
 
-    QString baseInfo;//基础信息
-    QString extraInfo;//额外信息
-    bool foldState;//折叠状态
-    FoldShapeStyle foldStyle;//折叠类型
-    int level;//当前层级
-    QList<TreeNode*> child;
-    TreeNode(QObject *parent = 0);
-    TreeNode(const QString& baseInfo, const QString& extraInfo, QObject *parent = 0);
-    ~TreeNode();
-    void addChild(TreeNode* child);
+private:
+    void Init();
+
+    FoldShapeStyle m_style;
+    QColor m_normalColor;
+    QColor m_selectedColor;
+    bool m_isFold;//折叠状态
+    bool m_isSelected;//选中
+
+    QPolygonF m_crossPolygon;
 };
+Q_DECLARE_METATYPE(FoldShape)
+
+class TreeNode
+{
+public:
+    TreeNode();
+    TreeNode(FoldShape foldShape, const QString& baseInfo, const QString& extraInfo);
+    ~TreeNode();
+    void addChild(TreeNode* node);
+    QColor normalColor() const { return m_normalColor; }
+    QColor hoverColor() const { return m_hoverColor; }
+    QColor selectedColor() const { return m_selectedColor; }
+    FoldShape foldShape() const { return m_foldShape; }
+
+private:
+    FoldShape m_foldShape;//折叠形状
+    QString m_baseInfo;//基础信息
+    QString m_extraInfo;//额外信息
+
+    QColor m_normalColor;
+    QColor m_hoverColor;
+    QColor m_selectedColor;
+    int m_level;//当前层级
+    QList<TreeNode*> child;//子元素
+};
+Q_DECLARE_METATYPE(TreeNode)
+
+class TreeNodeDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    TreeNodeDelegate(QWidget *parent = 0) : QStyledItemDelegate(parent) {}
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
+};
+
+
 
 class TreeNavigationBar : public QWidget
 {
